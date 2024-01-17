116693c9321b4a32da1fd75aeafd54dd9faa8fad
Marian Herrera <64176067+saalej@users.noreply.github.com>
2024-01-17 03:06:34 -0700

    CRUD - Type equipment

--- addons/Dispositivos/__manifest__.py
+++ addons/Dispositivos/__manifest__.py
@@ -5,7 +5,8 @@
     "depends": ['base'],
     "data" : [
         'Views/views_res.user.xml',
-        'Views/views_equipos.xml',
+        'Views/views_equipments.xml',
+        'Views/views_types_equipment.xml',
         'Views/views_os.xml',
         'security.xml'
     ],
